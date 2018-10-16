17bd068922e33eaeb1a5d1b37d9f5452a4e29dc5
madhurajput <42752696+madhurajput@users.noreply.github.com>
2018-10-16 07:22:56 +0530

    Update config.php

--- carrentalproject/includes/config.php
+++ carrentalproject/includes/config.php
@@ -14,3 +14,17 @@ catch (PDOException $e)
 exit("Error: " . $e->getMessage());
 }
 ?>
+'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'default'),
+            'username' => env('DB_USERNAME', 'default'),
+            'password' => env('DB_PASSWORD', 'secret'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
