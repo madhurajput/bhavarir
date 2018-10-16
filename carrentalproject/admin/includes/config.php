50cce8959200851bbec5d8aa0db3ec4db54785e7
madhurajput <42752696+madhurajput@users.noreply.github.com>
2018-10-16 07:31:33 +0530

    Update config.php

--- carrentalproject/admin/includes/config.php
+++ carrentalproject/admin/includes/config.php
@@ -1,31 +1,17 @@
 <?php 
 // DB credentials.
 
-define('DB_HOST','127.0.0.1:3306);
+define('DB_HOST','localhost:3306);
 define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','carrental');
 // Establish database connection.
 try
 {
-$dbh = new PDO("mysql:host=".DB_HOST.";DB_PORT=8889;dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+$dbh = new PDO("mysql:host=".DB_HOST.";DB_PORT=3306;dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 }
 catch (PDOException $e)
 {
 exit("Error: " . $e->getMessage());
 }
 ?>
-'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'default'),
-            'username' => env('DB_USERNAME', 'default'),
-            'password' => env('DB_PASSWORD', 'secret'),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
