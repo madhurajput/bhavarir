5d035fc9b4c556ffd711999e625341d30217210b
madhurajput <42752696+madhurajput@users.noreply.github.com>
2018-10-16 06:46:50 +0530

    Update config.php

--- carrentalproject/admin/includes/config.php
+++ carrentalproject/admin/includes/config.php
@@ -3,7 +3,7 @@
 
 define('DB_HOST','127.0.0.1:3306);
 define('DB_USER','root');
-define('DB_PASS','root');
+define('DB_PASS','');
 define('DB_NAME','carrental');
 // Establish database connection.
 try
