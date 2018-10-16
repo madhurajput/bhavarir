a6cc68ec4b00ab5719a3ac72cabacdc198231ce3
madhurajput <42752696+madhurajput@users.noreply.github.com>
2018-10-16 06:19:36 +0530

    Update config.php

--- carrentalproject/includes/config.php
+++ carrentalproject/includes/config.php
@@ -1,7 +1,6 @@
 <?php 
 // DB credentials.
-define('DB_HOST','127.0.0.1:3306', //or localhost);
-define('DB_PORT','3306');
+define('DB_HOST','127.0.0.1:3306');
 define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','carrental');
